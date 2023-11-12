869c695a5a806ac8dd44494683e4dbcef37d1895
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-11-12 14:29:57 +0000

    Update EmailResponse.jsx

--- app/components/EmailResponse.jsx
+++ app/components/EmailResponse.jsx
@@ -29,8 +29,8 @@ const EmailResponse = ({ message, firstname, lastname, email, subject }) => {
             <Section className="bg-white my-10 py-4 pt-2 rounded-lg shadow-md w-full border-4">
               <div className="w-full border-2">
                 <img
-                  width={120}
-                  height={120}
+                  width={121}
+                  height={121}
                   src="https://i.ibb.co/Mh69jcS/SianoFC.png"
                   alt="Siano FC Logo"
                   className="object-contain"
